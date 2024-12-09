70b20092fdf2699b553403e0f2e41ab50d710bae
diegovillegas <diego.villegas@menloschool.org>
2024-12-08 21:46:00 -0800

    commit #8

--- src/TextCompressor.java
+++ src/TextCompressor.java
@@ -43,123 +43,41 @@ public class TextCompressor {
         while (index < s.length()){
             // Get char
             prefix += s.charAt(index);
+            int counter = 1;
+            // Continue adding chars to the prefix if possible
+            while(tst.lookup(prefix) != - 1 && index + counter < s.length()){
+                prefix += s.charAt(index + counter);
+                counter++;
+            }
+            // Look for the prefix code
             // Check if it not in the trie
-            if(tst.lookup(prefix) == -1){
-                // Check if it is single letter
-                if(prefix.length() == 1){
-                    char letter = prefix.charAt(0);
-                    tst.insert(prefix, letter);
-                }
-                // Don't need this
-                else{
-                    tst.insert(prefix, customCode);
-                     customCode++;
-                }
+            if(prefix.length() == 1){
+                char letter = prefix.charAt(0);
+                tst.insert(prefix, letter);
+                BinaryStdOut.write(letter, 12);
             }
             else{
                 // Write out the code associated with prefix as 12 bits
                 BinaryStdOut.write(tst.lookup(prefix), 12);
             }
+            /*
+            if(tst.lookup(prefix) == -1){
+                // Check if it is single letter
+            }
+
+             */
             // If possible look ahead onto the next character
-
-        }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        /*
-        // Representing letters with a 6 bit code
-        int[] codes4Letters= new int['z' + 1];
-        int code = 0b000000;
-        // For loop which gives each letter a unique code
-        for(int letter = 'A'; letter < codes4Letters.length; letter++){
-            codes4Letters[letter] = code;
-            code += 0b000001;
-        }
-        // Code for space = Escape character
-        code += 0b000001;
-        codes4Letters[' '] = code;
-        // Apostrophe code
-        code += 0b000001;
-        codes4Letters[39] = code;
-        // Representing the 15 most common words with a 10 bit code
-        HashMap<String, Integer> codes4Words = new HashMap<String, Integer>();
-        // Starting from 63 so that it doesn't have the same value as the 6 bit codes
-        int wordCode = 0b0000111111;
-        codes4Words.put("the", wordCode);
-        wordCode += 0b0000000001;
-        codes4Words.put("be", wordCode);
-        wordCode += 0b0000000001;
-        codes4Words.put("to", wordCode);
-        wordCode += 0b0000000001;
-        codes4Words.put("of", wordCode);
-        wordCode += 0b0000000001;
-        codes4Words.put("and", wordCode);
-        wordCode += 0b0000000001;
-        codes4Words.put("in", wordCode);
-        wordCode += 0b0000000001;
-        codes4Words.put("that", wordCode);
-        wordCode += 0b0000000001;
-        codes4Words.put("have", wordCode);
-        wordCode += 0b0000000001;
-        codes4Words.put("it", wordCode);
-        wordCode += 0b0000000001;
-        codes4Words.put("for", wordCode);
-        wordCode += 0b0000000001;
-        codes4Words.put("not", wordCode);
-        wordCode += 0b0000000001;
-        codes4Words.put("with", wordCode);
-        wordCode += 0b0000000001;
-        codes4Words.put("you", wordCode);
-        wordCode += 0b0000000001;
-        codes4Words.put("he", wordCode);
-        wordCode += 0b0000000001;
-        codes4Words.put("as", wordCode);
-        // Convert all chars into a single string
-        String s = BinaryStdIn.readString();
-        String word = "";
-        for(int i = 0; i < s.length(); i++){
-            // Continue adding letters until we reach the end
-            if(i != ' '){
-                word += i;
+            if(index != s.length() - 2){
+                prefix += s.charAt(index + 1);
+                // Insert new prefix
+                tst.insert(prefix, customCode);
+                customCode++;
             }
-            else{
-                // Check if in HashMap, if so, write the 10 bit code
-                if(codes4Words.containsKey(word)){
-                    // Write escape key to let the expand method know when to read 10 bits
-                    BinaryStdOut.write(codes4Letters[' '], 6);
-                    BinaryStdOut.write(codes4Words.get(word), 10);
-                    // Reset word
-                    word = "";
-                }
-                else{
-                    // Convert letters to 6 bit codes
-                    for(int j = 0; j < word.length(); j++){
-                        BinaryStdOut.write(codes4Letters[word.charAt(i)], 6);
-                    }
-                    // Reset word
-                    word = "";
-                }
-            }
+            index += prefix.length();
         }
+        // Add EOF as 80 to signal the end for expand function
+        BinaryStdOut.write(80, 12);
         BinaryStdOut.close();
-
-         */
     }
 
     private static void expand() {
